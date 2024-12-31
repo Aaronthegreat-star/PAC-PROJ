8bb88faac11895b4d66089b5c511fa2f1704005d
Aaronthegreat-star <aaronhood38@gmail.com>
2024-12-31 05:00:06 +0100

    new update

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,25 +1,23 @@
 # terraform {
-#   backend "s3" {
-#     bucket = "api-terraform-state-file"
-#     key = "terraform/state/terraform.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "terraform-state-locking"
-#     encrypt = true
-#   }
-# }
+# #   backend "s3" {
+# #     bucket = "api-terraform-state-file-bucket"
+# #     key = "terraform/state/terraform.tfstate"
+# #     region = "us-east-1"
+# #     dynamodb_table = "terraform-state-lock"
+# #     encrypt = true
+# #   }
+# # }
 
 # terraform {
 #     backend "remote" {
-#         hostname = "app.terraform.io"
-#         organization = "Aaronhood"
+#     = "Aaronhood"
 
 #         workspaces {
 #             name = "PAC-PROJ"
 #         }
 #     }
 # }
-
 # resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "api-terraform-state-file-bucket"
-#   region = "us-east-1"
+#    bucket = "api-terraform-state-file-bucket"
+# #    region = "us-east-1"
 # }
